652905a699ea535955624ee6772377cdc905d4d7
NadiiaMatsko <matsko.nadya@gmail.com>
2021-09-07 18:31:46 +0300

    hw12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log(55555);
+'use strict';
+
+import './sass/main.scss';
+
+import fetchCountries from './js/fetchCountries';
+import coutryListMarkup from './templates/countries.hbs';
+import coutryInfoMarkup from './templates/info.hbs';
+import { refs } from './js/refs';
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import { defaults } from '@pnotify/core';
+defaults.delay = '1500';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const debounce = require('lodash.debounce');
+const URL_BASE = 'https://restcountries.eu/rest/v2/name';
+
+const clearRender = () => {
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = '';
+};
+
+const countryRender = countries => {
+  if (countries.length === 1) {
+    clearRender();
+    refs.info.innerHTML = coutryInfoMarkup(countries);
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    clearRender();
+    refs.list.innerHTML = coutryListMarkup(countries);
+  } else {
+    clearRender();
+    error({
+      text: 'Too many matches found. Please enter a more specific query!',
+    });
+  }
+};
+
+function onInput(e) {
+  const searchQuery = e.target.value;
+  fetchCountries(searchQuery)
+    .then(countryRender)
+    .catch(err => {
+      clearRender();
+      error({
+        text: 'Please enter a valid country name!',
+      });
+    });
+}
+refs.input.addEventListener('input', debounce(onInput, 500));
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  input: document.querySelector('#js-input'),
+  list: document.querySelector('.js-list'),
+  info: document.querySelector('.country-info'),
+};
